f160bdb04be3188b2b867cc507564a7d450b5e97
John Bachir <j@jjb.cc>
2020-04-12 03:04:11 -0400

    more working code for individual country
    (this code as committed doesn't run but I just
    wanted to commit it in this state before making mroe changes)

--- code.rb
+++ code.rb
@@ -1,4 +1,14 @@
-# https://covid.ourworldindata.org/data/ecdc/total_deaths.csv
+require "open-uri"
+require "csv"
+require 'gruff'
+require 'pry'
+
+file = open('https://covid.ourworldindata.org/data/ecdc/total_deaths.csv')
+# csv_string = file.read
+# csv = CSV.new(csv_string, headers: true)
+csv = CSV.read(file.path, headers: true)
+countries= ['United States', 'France']
+
 
 def moving_average(array, num: 7, start: 7, round: 100)
   a = Array.new(start-1, 0)
@@ -18,27 +28,27 @@ def slope(array, start=8)
   a
 end
 
-dates = %w[3/5/2020 3/6/2020 3/7/2020 3/8/2020 3/9/2020 3/10/2020 3/11/2020 3/12/2020 3/13/2020 3/14/2020 3/15/2020 3/16/2020 3/17/2020 3/18/2020 3/19/2020 3/20/2020 3/21/2020 3/22/2020 3/23/2020 3/24/2020 3/25/2020 3/26/2020 3/27/2020 3/28/2020 3/29/2020 3/30/2020 3/31/2020 4/1/2020 4/2/2020 4/3/2020 4/4/2020 4/5/2020 4/6/2020 4/7/2020 4/8/2020 4/9/2020 4/10/2020 4/11/2020]
+# dates = %w[3/5 3/6 3/7 3/8 3/9 3/10 3/11 3/12 3/13 3/14 3/15 3/16 3/17 3/18 3/19 3/20 3/21 3/22 3/23 3/24 3/25 3/26 3/27 3/28 3/29 3/30 3/31 4/1 4/2 4/3 4/4 4/5 4/6 4/7 4/8 4/9 4/10 4/11]
+dates = csv['date']
+start_day=14
+dates = dates[start_day-1..]
 h = {}
 0.upto(dates.size-1) do |i|
   h[i]=dates[i]
 end
 dates = h
+i=0
+dates = dates.select!{keep=0==i%5 ; i+=1; keep}
+
+
 new_deaths = [3.0,2.0,7.0,3.0,11.0,3.0,15.0,13.0,18.0,12.0,36.0,21.0,27.0,89.0,108.0,78.0,112.0,112.0,186.0,240.0,231.0,365.0,299.0,319.0,292.0,418.0,499.0,509.0,471.0,588.0,1053.0,518.0,833.0,1417.0,541.0,1341.0,987.0,635.0]
 ma = moving_average(new_deaths)
 ma_slope = slope(ma)
 ma_slope_ma = moving_average(ma_slope, start: 14, round: 2)
-# puts new_deaths
-# puts "---"
-# puts ma
-# puts "---"
-# puts ma_slope
-# puts ma_slope_ma
-require 'gruff'
-require 'pry'
 g = Gruff::Line.new(1400)
+binding.pry
 g.y_axis_increment = 10
-# g.labels = dates
-g.data :thing, ma_slope_ma[13..]
-g.minimum_x_value = -50
+# g.minimum_value = 0
+g.labels = dates
+g.data :thing, ma_slope_ma[start_day-1..]
 g.write('new.png')
