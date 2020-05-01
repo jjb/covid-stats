4728980656cec95ad974e99c3a56cb42c26d6c9e
John Bachir <j@jjb.cc>
2020-04-30 20:43:23 -0400

    no need to mutate array

--- nyc.rb
+++ nyc.rb
@@ -6,6 +6,6 @@ class NYC
     nyc_file = Down.download('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/case-hosp-death.csv')
     nyc_csv = CSV.read(nyc_file.path, headers: true)
     nyc_deaths = nyc_csv['DEATH_COUNT']
-    @data = nyc_deaths.map!{|n| 'null' == n ? 0.0 : n.to_f }
+    @data = nyc_deaths.map{|n| 'null' == n ? 0.0 : n.to_f }
   end
 end
